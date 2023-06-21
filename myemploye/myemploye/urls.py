130054a3980b150bcd4329649d075bf3a53182e8
Shantanu Raut <rshantanu73@gmail.com>
2023-06-22 00:05:23 +0530

    New user add with defalut profile pics

--- myemploye/myemploye/urls.py
+++ myemploye/myemploye/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.contrib.auth import views as auth_views #This is for the login and logout page
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from users import views as user_views
 
 urlpatterns = [
@@ -27,3 +29,6 @@ urlpatterns = [
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'), #This is the path for the logout page
     path('', include('members.urls')),        #This is also blank for the blank port also with the forder name members and urls.py
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
